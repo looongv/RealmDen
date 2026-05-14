986cabaa3e6e1b8927dc6ce734b2310cbccb8e08
Long <3028606@gmail.com>
2026-05-13 18:21:10 -0700

    set up spring boot backend and minecraft test docker compose

---
+++ backend/src/main/java/com/realmden/api/RealmdenApiApplication.java
@@ -0,0 +1,13 @@
+package com.realmden.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmdenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmdenApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/realmden/api/RealmdenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realmden.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmdenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
